56c612d0cc07462f22d76850c1ab285de105f1e1
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2024-04-26 19:26:29 +0200

    fix ua

--- src/index.ts
+++ src/index.ts
@@ -65,12 +65,12 @@ async function main() {
         'priority': 'u=1, i',
         'origin': 'https://www.base.org',
         'referer': 'https://www.base.org/builder-anniversary-nft',
-        'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"macOS"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
+        'sec-fetch-site': 'same-origin',
       },
     })
 
